3e9d2b4db8552d7cef670fd9de7755096982133a
Paul Mühlbacher <35687048+phaulson@users.noreply.github.com>
2018-03-12 12:01:57 +0100

    Update createTables.sql

--- createTables.sql
+++ createTables.sql
@@ -1,4 +1,4 @@
-create table user
+create table sp_user
 (
     idUser integer Primary Key,
     username varchar2(20),
@@ -6,26 +6,26 @@ create table user
     biographie varchar2(500),
     isPro number(1,0)
 );
-create table post
+create table sp_post
 (
     idPost integer Primary Key,
     idCreator integer,
     caption varchar2(500),
     likes integer,
     timestamp date,
-    constraint fk_post_user foreign key(idCreator) references user(idUser)
+    constraint fk_post_user foreign key(idCreator) references sp_user(idUser)
 );
-create table comment
+create table sp_comment
 (
     idComment integer primary key,
     idCreator integer,
     idPost integer,
     content varchar2(100),
     timestamp date,
-    constraint fk_comment_user foreign key(idCreator) references User(idUser),
-    constraint fk_comment_post foreign key(idPost) references Post(idPost)
+    constraint fk_comment_user foreign key(idCreator) references sp_user(idUser),
+    constraint fk_comment_post foreign key(idPost) references sp_post(idPost)
 );
-create table location
+create table sp_location
 (
     idLocation integer primary key,
     idPage integer,
@@ -35,64 +35,64 @@ create table location
     type varchar2(20) check(type in ('EVENT', 'PUBLIC_PLACE', 'GYM', 'OTHER')),
     startdatetime date,
     enddatetime date,
-    constraint fk_location_page foreign key(idPost) references Post(idPost),
+    constraint fk_location_page foreign key(idPost) references sp_post(idPost),
     constraint ck_location check(startdatetime < enddatetime and 
       ((startdatetime is null and enddatetime is null and type not like 'EVENT') or
       (startdatetime is not null and enddatetime is not null and type = 'EVENT')))
 );
-create table follows 
+create table sp_follows 
 (
     idFollower integer,
     idOl integer,
     constraint pk_follows primary key (idFollower, idOl),
-    constraint fk_follows_follower foreign key(idFollower) references user(idUser),
-    constraint fk_follows_ol foreign key(idOl) references user(idUser),
+    constraint fk_follows_follower foreign key(idFollower) references sp_user(idUser),
+    constraint fk_follows_ol foreign key(idOl) references sp_user(idUser),
     constraint ck_follows check (idFollower != idOl)
 );
-create table exercise (
+create table sp_exercise (
     idExercise integer primary key,
     name varchar2(50),
     description varchar2(500)
 );
-create table workout (
+create table sp_workout (
     idWorkout integer primary key,
     name varchar2(50)
 );
-create table dailyWorkout (
+create table sp_dailyWorkout (
     idDailyWorkout integer primary key,
     name varchar2(50)
 );
-create table plan (
+create table sp_plan (
     idPlan integer primary key,
     idCreator integer,
     name varchar2(50),
-    constraint fk_plan_creator foreign key(idCreator) references user(idUser)
+    constraint fk_plan_creator foreign key(idCreator) references sp_user(idUser)
 );
-create table subscription (
+create table sp_subscription (
     idPlan integer,
     idUser integer,
     constraint pk_subscription primary key (idUser, idPlan),
-    constraint fk_subscription_user foreign key(idUser) references user(idUser),
-    constraint fk_subscription_plan foreign key(idPlan) references plan(idPlan)
+    constraint fk_subscription_user foreign key(idUser) references sp_user(idUser),
+    constraint fk_subscription_plan foreign key(idPlan) references sp_plan(idPlan)
 );
-create table containsPD (
+create table sp_containsPD (
     idPlan integer,
     idDailyWorkout integer,
     constraint pk_containsPD primary key (idDailyWorkout, idPlan),
-    constraint fk_containsPD_dailyWorkout foreign key(idDailyWorkout) references dailyWorkout(idDailyWorkout),
-    constraint fk_containsPD_plan foreign key(idPlan) references plan(idPlan)
+    constraint fk_containsPD_dailyWorkout foreign key(idDailyWorkout) references sp_dailyWorkout(idDailyWorkout),
+    constraint fk_containsPD_plan foreign key(idPlan) references sp_plan(idPlan)
 );
-create table containsDW (
+create table sp_containsDW (
     idDailyWorkout integer,
     idWorkout integer,
     constraint pk_containsDW primary key (idDailyWorkout, idWorkout),
-    constraint fk_containsDW_dailyWorkout foreign key(idDailyWorkout) references dailyWorkout(idDailyWorkout),
-    constraint fk_containsDW_workout foreign key(idWorkout) references workout(idWorkout)
+    constraint fk_containsDW_dailyWorkout foreign key(idDailyWorkout) references sp_dailyWorkout(idDailyWorkout),
+    constraint fk_containsDW_workout foreign key(idWorkout) references sp_workout(idWorkout)
 );
-create table containsWE (
+create table sp_containsWE (
     idWorkout integer,
     idExercise integer,
     constraint pk_containsWE primary key (idExercise, idWorkout),
-    constraint fk_containsWE_exercise foreign key(idExercise) references exercise(idExercise),
-    constraint fk_containsWE_workout foreign key(idWorkout) references workout(idWorkout)
+    constraint fk_containsWE_exercise foreign key(idExercise) references sp_exercise(idExercise),
+    constraint fk_containsWE_workout foreign key(idWorkout) references sp_workout(idWorkout)
 );
