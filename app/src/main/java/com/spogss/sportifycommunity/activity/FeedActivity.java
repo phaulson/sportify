96a498c9d02578f9858a656e726219c7d8cc180c
Paul Mühlbacher <pmuehlbacher13@gmail.com>
2018-03-26 11:13:03 +0200

    update menu

--- app/src/main/java/com/spogss/sportifycommunity/activity/FeedActivity.java
+++ app/src/main/java/com/spogss/sportifycommunity/activity/FeedActivity.java
@@ -105,17 +105,18 @@ public class FeedActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
 
-        } else if (id == R.id.nav_slideshow) {
+        // TODO: implement onMenuItemClick
+        if(id == R.id.nav_home) {
 
-        } else if (id == R.id.nav_manage) {
+        }
+        else if (id == R.id.nav_plans) {
 
-        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_map) {
 
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_settings) {
+
+        } else if (id == R.id.nav_logout) {
 
         }
 
